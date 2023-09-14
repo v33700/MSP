c321444c0ef487dd1f4033c4d6a58407ca3f8391
Jacqui <102585266@student.swin.edu.au>
2023-09-14 19:02:16 +1000

    Added Database Class

---
+++ test1_Aidan/DraftGotoGro/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraftGotoGro
+{
+    public class Database
+    {
+        private string _connection;
+
+        public string Conn { get { return _connection; } }
+
+        Database(string connection) 
+        {
+            _connection = connection;
+        }
+
+    }
+}
--- test1_Aidan/DraftGotoGro/obj/Debug/net6.0-windows/SalesInput.g.i.cs
+++ test1_Aidan/DraftGotoGro/obj/Debug/net6.0-windows/SalesInput.g.i.cs
@@ -47,13 +47,13 @@ namespace DraftGotoGro {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DraftGotoGro;component/salesinput.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DraftGotoGro;V1.0.0.0;component/salesinput.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\SalesInput.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -63,7 +63,7 @@ namespace DraftGotoGro {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
