dd5ab257adaa36a17057ca74a0a75aba0a61fc2d
nprez <nprez@DESKTOP-UB1KI4V>
2023-09-19 22:41:20 +1000

    Made sure that data entered is in correct format.

--- test1_Aidan/DraftGotoGro/SalesInput.xaml.cs
+++ test1_Aidan/DraftGotoGro/SalesInput.xaml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -27,13 +28,45 @@ namespace DraftGotoGro
         }
         private void SubmitButton_Click(object sender, RoutedEventArgs e)
         {
-            if (MemberIDBox.Text == "" || OrderNumber.Text == "" || ItemID.Text == "" || ItemQuantity.Text == "");
+            if (MemberIDBox.Text == "" || OrderNumber.Text == "" || ItemID.Text == "" || ItemQuantity.Text == "") ;
             {
                 SaleErrorLabel.Content = "Please fill all the data.";
                 SaleErrorLabel.Visibility = Visibility.Visible;
                 return;
             }
+            string pattern = "^[A-Z]{4}d{2}"; //Four capital letters and 2 digit numbers
+            bool isMatch = Regex.IsMatch(MemberIDBox.Text, pattern);
+            if (!isMatch)
+            {
+                SaleErrorLabel.Content = "Member ID is not of a valid format. Please try again.";
+                SaleErrorLabel.Visibility = Visibility.Visible;
+                return;
+            }
+            pattern = "^d{5}"; //
+            isMatch = Regex.IsMatch(OrderNumber.Text, pattern);
+            if (!isMatch)
+            {
+                SaleErrorLabel.Content = "Order number is not of a valid format. Please try again.";
+                SaleErrorLabel.Visibility = Visibility.Visible;
+                return;
+            }
+            pattern = "^d{3}"; //
+            isMatch = Regex.IsMatch(ItemID.Text, pattern);
+            if (!isMatch)
+            {
+                SaleErrorLabel.Content = "Item ID is not of a valid format. Please try again.";
+                SaleErrorLabel.Visibility = Visibility.Visible;
+                return;
+            }
+            pattern = "^d{2}"; //
+            isMatch = Regex.IsMatch(ItemQuantity.Text, pattern);
+            if (!isMatch)
+            {
+                SaleErrorLabel.Content = "Item Quantity is not of a valid format. Please try again.";
+                SaleErrorLabel.Visibility = Visibility.Visible;
+                return;
+            }
         }
-
+    }
 
 }
