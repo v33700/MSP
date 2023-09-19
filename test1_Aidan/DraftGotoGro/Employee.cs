7e02abd9019a679a3bf9ff04e46fa480908571aa
AidanGrimmettSwin <141190734+AidanGrimmettSwin@users.noreply.github.com>
2023-09-19 19:04:48 +1000

    Login page layout + employee class
    Added employee login page xaml layout to a blank VS solution. Also made an employee class (employee.cs)

--- test1_Aidan/DraftGotoGro/Employee.cs
+++ test1_Aidan/DraftGotoGro/Employee.cs
@@ -15,11 +15,11 @@ namespace DraftGotoGro
         public string lastName { get; set; }
         public string password { get; set; }
         public string email { get; set; }
-        public string phone { get; set; }
+        public int phone { get; set; }
         public DateTime birthday { get; set; }
 
         // Constructor
-        public Employee(int ID, string Username, string FirstName, string LastName, string Password, string Email, string Phone, DateTime Birthday)
+        public Employee(int ID, string Username, string FirstName, string LastName, string Password, string Email, int Phone, DateTime Birthday)
         {
             employeeID = ID;
             username = Username;
