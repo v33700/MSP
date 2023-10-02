e8cf386cebf3f26bf7e8a6a8e3c6b176e16717a7
THOMAS PETTIGREW <101615515@student.swin.edu.au>
2023-10-02 18:00:25 +1100

    CSV Gen Class creation and basic functionality

---
+++ test1_Aidan/DraftGotoGro/CSVGEN.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Linq;
+using System.Text;
+
+namespace DraftGotoGro
+{
+    internal class CSVGEN
+    {
+        private List<Dictionary<string, object>> data;
+
+        public CSVGEN(string jsonData)
+        {
+            data = JsonSerializer.Deserialize<List<Dictionary<string, object>>>(jsonData);
+
+        }
+
+        public void ToCsv(string filename)
+        {
+            if (data == null || !data.Any())
+            {
+                System.Console.WriteLine("No data provided!");
+                return;
+            }
+
+            StringBuilder csvContent = new StringBuilder();
+
+            // Extract the header from the keys of the first dictionary
+            var header = data[0].Keys;
+            csvContent.AppendLine(string.Join(",", header));
+
+            foreach (var record in data)
+            {
+                var line = string.Join(",", record.Values.Select(v => v.ToString()));
+                csvContent.AppendLine(line);
+            }
+
+            File.WriteAllText(filename, csvContent.ToString());
+        }
+
+    }
+}
