ef9cf192b666d466f35dff74ad4067c7322b138c
THOMAS PETTIGREW <101615515@student.swin.edu.au>
2023-10-05 21:30:00 +1100

    Added the return features for the CSV gen  based on radio button selected.

--- test1_Aidan/DraftGotoGro/Report.xaml.cs
+++ test1_Aidan/DraftGotoGro/Report.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,45 +26,94 @@ namespace DraftGotoGro
     /// </summary>
     public partial class Report : Page
     {
-        //private ObservableCollection<Member> MemberResult = new ObservableCollection<Member>();
-        //private ObservableCollection<Sale> SaleResult = new ObservableCollection<Sale>();
+        private ObservableCollection<Member> MemberResult = new ObservableCollection<Member>();
+        private ObservableCollection<Sale> SaleResult = new ObservableCollection<Sale>();
 
-        //private IMongoDatabase _database;
-        //private IMongoCollection<Member> _memberCollection;
+        private IMongoDatabase _database;
+       private IMongoCollection<Member> _memberCollection;
 
-        //private IMongoCollection<Sale> _saleCollection; // Add this to the class definition
+        private IMongoCollection<Sale> _saleCollection; // Add this to the class definition
 
         public Report()
         {
             InitializeComponent();
-            //var client = new MongoClient("mongodb+srv://SWECLASS:IXo4LdFQqKUdJXIr@tomstestcluster.unrd1c2.mongodb.net/"); // TODO: Secure the connection string!
-            //_database = client.GetDatabase("SWE");
-            //_memberCollection = _database.GetCollection<Member>("Members");
-            //_saleCollection = _database.GetCollection<Sale>("Sales"); // Initialize the sale collection
+            var client = new MongoClient("mongodb+srv://SWECLASS:IXo4LdFQqKUdJXIr@tomstestcluster.unrd1c2.mongodb.net/"); // TODO: Secure the connection string!
+            _database = client.GetDatabase("SWE");
+            _memberCollection = _database.GetCollection<Member>("Members");
+            _saleCollection = _database.GetCollection<Sale>("Sales"); // Initialize the sale collection
         }
 
         private void MemberReportBtn_Click(object sender, RoutedEventArgs e)
         {
-            //if (WeekRadio.IsChecked == true)
-            //{
-            //    DateTime oneWeekAgo = DateTime.UtcNow.AddDays(-7);  
+            if (WeekRadio.IsChecked == true)
+            {
+                DateTime oneWeekAgo = DateTime.UtcNow.AddDays(-7);
 
-          
-            //    var filter = Builders<Sale>.Filter.Gte(s => s.SaleDate, oneWeekAgo);
-            //    var sales = _saleCollection.Find(filter).ToList();
+                var filter = Builders<Sale>.Filter.Gte(s => s.saleDate, oneWeekAgo);
+                var sales = _saleCollection.Find(filter).ToList();
 
-            //    var uniqueMemberIds = sales.Select(s => s.MemberID).Distinct().ToList();
+                var uniqueMemberIds = sales.Select(s => s.MemberID).Distinct().ToList();
 
+                var memberFilter = Builders<Member>.Filter.In(m => m.Id, uniqueMemberIds);
+                var membersFromSales = _memberCollection.Find(memberFilter).ToList();
+                using (var ms = new MemoryStream())
+                {
+                    using (var writer = new BsonBinaryWriter(ms))
+                    {
+                        BsonSerializer.Serialize(writer, membersFromSales);
+                    }
 
-            //    var memberFilter = Builders<Member>.Filter.In(m => m.Id, uniqueMemberIds);
-            //    var membersFromSales = _memberCollection.Find(memberFilter).ToList();
+                     string jsonData = Encoding.UTF8.GetString(ms.ToArray());
+                    CSVGEN csvGenerator = new CSVGEN(jsonData);
+                    csvGenerator.ToCsv("output.csv");
+                }
+            }
+            if (MonthRadio.IsChecked == true)
+            {
+                DateTime oneMonthAgo = DateTime.UtcNow.AddDays(-30);
 
-            //    // Now, you have the list of members from sales in the past week. Convert this to JSON
-            //    string jsonData = JsonConvert.SerializeObject(membersFromSales);
+                var filter = Builders<Sale>.Filter.Gte(s => s.saleDate, oneMonthAgo);
+                var sales = _saleCollection.Find(filter).ToList();
 
-            //    CSVGEN csvGenerator = new CSVGEN(jsonData);
-            //    csvGenerator.ToCsv("output.csv");
-            //}
+                var uniqueMemberIds = sales.Select(s => s.MemberID).Distinct().ToList();
+
+                var memberFilter = Builders<Member>.Filter.In(m => m.Id, uniqueMemberIds);
+                var membersFromSales = _memberCollection.Find(memberFilter).ToList();
+                using (var ms = new MemoryStream())
+                {
+                    using (var writer = new BsonBinaryWriter(ms))
+                    {
+                        BsonSerializer.Serialize(writer, membersFromSales);
+                    }
+
+                    string jsonData = Encoding.UTF8.GetString(ms.ToArray());
+                    CSVGEN csvGenerator = new CSVGEN(jsonData);
+                    csvGenerator.ToCsv("output.csv");
+                }
+            }
+            if (AllRadio.IsChecked == true)
+            {
+                            var sales = _saleCollection.Find(_ => true).ToList();
+
+                var uniqueMemberIds = sales.Select(s => s.MemberID).Distinct().ToList();
+
+                var memberFilter = Builders<Member>.Filter.In(m => m.Id, uniqueMemberIds);
+                var membersFromSales = _memberCollection.Find(memberFilter).ToList();
+
+                using (var ms = new MemoryStream())
+                {
+                    using (var writer = new BsonBinaryWriter(ms))
+                    {
+                        BsonSerializer.Serialize(writer, membersFromSales);
+                    }
+
+                    string jsonData = Encoding.UTF8.GetString(ms.ToArray());
+                    CSVGEN csvGenerator = new CSVGEN(jsonData);
+                    csvGenerator.ToCsv("output.csv");
+                }
+
+            }
+        }
         }
 
     }
--- test1_Aidan/DraftGotoGro/Sale.cs
+++ test1_Aidan/DraftGotoGro/Sale.cs
@@ -8,7 +8,7 @@ namespace DraftGotoGro
 {
     public class Sale
     {
-        public string MemberID { get; set; }
+        public int MemberID { get; set; }
         public int OrderNumber { get; set; }
         public List<Item> Items { get; set; }
         public DateTime saleDate { get; set; }
