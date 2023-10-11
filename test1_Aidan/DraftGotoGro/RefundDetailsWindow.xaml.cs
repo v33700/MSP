5f0d9206a97b2bdb61d5c817aeeef609f9c09755
NGUYEN HOANG NGHIA PHAN <103521795@student.swin.edu.au>
2023-10-11 20:49:03 +1100

    incomplete refund

--- test1_Aidan/DraftGotoGro/RefundDetailsWindow.xaml.cs
+++ test1_Aidan/DraftGotoGro/RefundDetailsWindow.xaml.cs
@@ -1,22 +1,47 @@
 ﻿using System.Windows;
+using MongoDB.Driver;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
 
 namespace DraftGotoGro
 {
     public partial class RefundDetailsWindow : Window
     {
-        public RefundDetailsWindow(Sale selectedSale)
+        private Sale _selectedSale;
+        private IMongoCollection<Sale> _saleCollection;
+
+        public RefundDetailsWindow(Sale selectedSale, IMongoCollection<Sale> saleCollection)
         {
             InitializeComponent();
+            _selectedSale = selectedSale;
+            _saleCollection = saleCollection;
             ItemsListView.ItemsSource = selectedSale.Items;
-            // Additional setup if necessary
         }
 
         private void RefundButton_Click(object sender, RoutedEventArgs e)
         {
-            // Implement logic to handle refund here.
+            if (sender is Button deleteButton)
+            {
+                if (deleteButton.DataContext is Item selectedItem)
+                {
+                    var filter = Builders<Sale>.Filter.Eq("_id", _selectedSale.Id);
+                    var update = Builders<Sale>.Update.PullFilter("Items", Builders<Item>.Filter.Eq("_id", selectedItem.Id));
+
+                    var result = _saleCollection.UpdateOne(filter, update);
+
+                    if (result.IsAcknowledged && result.ModifiedCount > 0)
+                    {
+                        _selectedSale.Items.Remove(selectedItem);
+                        ItemsListView.Items.Refresh();
+                        MessageBox.Show("Item refunded successfully.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to refund the item.");
+                    }
+                }
+            }
         }
-
-
-        
     }
 }
